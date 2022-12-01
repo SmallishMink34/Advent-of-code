df11bbe4c99ba9a02c693d1d1d141f8e979b3484
SmallIshMink <mattheo.m10@gmail.com>
2022-12-01 11:04:07 +0100

    1 jour

---
+++ Day 1/Main.py
@@ -0,0 +1,15 @@
+liste = open('Day 1/Elf_inv.txt', 'r')
+
+somme_liste = [0]
+indice_somme = 0
+for i in liste.readlines():
+    if i.strip() == '':
+        print(i)
+        indice_somme += 1
+        somme_liste.append(0)
+    else:
+        print(i)
+        somme_liste[indice_somme] += int(i.strip())
+        
+# Affiche la sommes des trois Elfs ayant le plus de Calories dans leurs Sac
+print(sorted(somme_liste)[-1]+sorted(somme_liste)[-2]+sorted(somme_liste)[-3])
